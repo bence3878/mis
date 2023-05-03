02cf212a8c9ddc409b35f5f00faeb92b787f5290
bence3878 <bence.szever@gmail.com>
2023-05-03 18:16:59 +0200

    alma

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ add-spt-repository -y ppa:remmina-ppa-team/remmina-next
 
 apt update
 
-apt install -ym mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola #mailutils
+apt install -y mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola inxi #mailutils

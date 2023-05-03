14d5fde254cb713ac876238022255aaf557170c4
bence3878 <bence.szever@gmail.com>
2023-05-03 16:46:51 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+apt upgrade
+
+add-spt-repository -y ppa:remmina-ppa-team/remmina-next
+
+apt update
+
+apt install -ym mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola #mailutils

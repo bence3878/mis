6a562b1daad64714d35c3559e399b2d0fe6d985d
Bence <bence.szever@gmail.com>
2023-05-03 22:54:24 +0200

    install.sh javitása

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 apt update
-apt upgrade
+apt upgrade -y
 
 add-spt-repository -y ppa:remmina-ppa-team/remmina-next
 
 apt update
 
-apt install -y mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola inxi #mailutils
+for i in mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola inxi; do
+apt install -y $i
+done

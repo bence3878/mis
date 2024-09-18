97fd0fb38fd755db4bee5b3d79035f6053c9c4e4
Pákozdi Bence <eidzokap@gmail.com>
2024-09-18 03:05:16 +0200

    új csomag felvétele

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ echo "csomag lista frisitése"
 apt upgrade -y #> /dev/null
 echo "csomagok frisitése"
 
-add-spt-repository -y ppa:remmina-ppa-team/remmina-next > /dev/null
-add-apt-repository -y ppa:lakinduakash/lwh > /dev/null
+add-spt-repository -y ppa:remmina-ppa-team/remmina-next #> /dev/null
+add-apt-repository -y ppa:lakinduakash/lwh #> /dev/null
 
 apt update #> /dev/null
-VALAMI='mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola inxi neofetch uex espanso linux-wifi-hotspot snap linux-wifi-hotspot guake '
+VALAMI=$(mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola inxi neofetch uex espanso linux-wifi-hotspot snap linux-wifi-hotspot guake fzf tree)
 for i in $VALAMI; do
-apt install -y $i >/dev/null
+apt install -y $i #>/dev/null
 echo $i "csomag telepitése"
 done

1cec3c613ddb01e81065776d472b9d7d9c92a205
Bence3878 <eidzokap@gmail.com>
2024-10-08 16:27:06 +0200

    fájlok törlése

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo -e "\e[31mppa tárolók hozzá addása\e[39;49m"
 apt update #> /dev/null
 echo -e "\e[31mcsomag lista frisitése\e[39;49m"
 
-VALAMI="mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola inxi neofetch uex espanso linux-wifi-hotspot snap linux-wifi-hotspot guake fzf tree gufw"
+VALAMI="mc zsh zsh-theme-powerlevel9k git curl gcc g++ clang gdb speedtest-cli remmina remmina-plugin-rdp remmina-plugin-secret gimp openssh-server sl cowsay lolcat w3m w3m-img exa ranger htop btop hexedit tmux numlockx terminator fonts-powerline wine git-cola inxi neofetch uex espanso linux-wifi-hotspot snap linux-wifi-hotspot guake fzf tree gufw default-jre"
 for i in $VALAMI; do
 apt install -y $i #>/dev/null
 echo -e "\e[31m"$i "csomag telepitése\e[39;49m"
